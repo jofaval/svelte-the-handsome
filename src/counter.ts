36cac11c9b189dd7e19e2db4a81e6da17510078d
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-18 21:12:09 +0100

    feat: Play with a reactive prop

--- src/counter.ts
+++ src/counter.ts
@@ -1,24 +1,9 @@
-type UnnecessaryClosureProps = {
-  element: HTMLButtonElement;
-  count: number;
-};
-
-const unnecessaryClosure = ({
-  element,
-  count,
-}: UnnecessaryClosureProps): void => {
-  element.innerHTML = `count is ${count}`;
-};
-
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0;
 
-  const setCounter = (count: number) => {
-    counter = count;
-    unnecessaryClosure({ element, count });
-  };
-  const increment = () => setCounter(counter + 1);
-  unnecessaryClosure({ element, count: counter });
+  const setCounter = (count: number) => (counter = count);
 
-  element.addEventListener("click", increment);
+  $: element.innerHTML = `count is ${counter}`;
+
+  element.addEventListener("click", () => setCounter(counter + 1));
 }
