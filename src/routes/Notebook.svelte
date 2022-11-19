30adbd5ad67c5d2eea4e869b4de99734155d678c
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-19 01:39:47 +0100

    feat: Create initial files

---
+++ src/routes/Notebook.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let text: string = '';
+</script>
+
+<div class="notebook">
+	<textarea name="" id="" cols="30" rows="10" />
+</div>
+
+<style>
+	.notebook {
+		border: thin solid black;
+	}
+</style>
